1ddd0b3e93c64a142f1bd012b312ae1d88cb98ed
vmmoorthy <58259989+vmmoorthy@users.noreply.github.com>
2025-05-19 18:27:32 +0530

    Update base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: process.env.CI ? '/sorting-algo/' : '/',
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });

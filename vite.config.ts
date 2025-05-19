a7b2f2a9572944d5e945e8c5cb02914ed7c979ce
vmmoorthy <vmmoorthy25@gmail.com>
2025-05-19 20:35:54 +0530

    Revert "Update base path"
    This reverts commit 1ddd0b3e93c64a142f1bd012b312ae1d88cb98ed.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: process.env.CI ? '/sorting-algo/' : '/',
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });

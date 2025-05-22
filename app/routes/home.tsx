9471a19b735a5929f013eb90c3b0be8e2f7e9439
vmmoorthy <vmmoorthy25@gmail.com>
2025-05-22 19:32:22 +0530

    migration_react_router

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -4,8 +4,7 @@ import { Main } from "../components/Main"
 export function meta({ }: Route.MetaArgs) {
   return [
     { title: "Illustrator-Sorting Algo" },
-    { name: "description", content: "Simulation for sorting Algotithms" },
-    // { rel: "icon", type: "image/png", href: "/icon.png" }
+    { name: "description", content: "Simulation for sorting Algotithms" }
   ];
 }
 

7ae467316d351c1a189fcf38779d749e3c0463d5
vmmoorthy <vmmoorthy25@gmail.com>
2025-06-09 13:31:12 +0530

    feat: Implement Selection Sort algorithm and integrate into main component

---
+++ app/components/SortAlgos/selectionSort.tsx
@@ -0,0 +1,100 @@
+import { useContext, useEffect, useRef } from "react";
+import { COLOR_COMPARE, COLOR_SORTED, COLOR_SWAP, COLOR_UNSORTED } from "~/constants";
+import { MainContext } from "~/contextProvider/MainContextProvider";
+import type { ElementType } from "~/types";
+import Controls from "../Controls";
+import useSelectionSort from "~/hooks/useSelectionSort";
+
+
+const getColor = (element: ElementType): string => {
+    if (element.isMarkedForSwap)
+        return COLOR_SWAP
+    if (element.isMarkedForCompare)
+        return COLOR_COMPARE
+    if (element.IsSorted)
+        return COLOR_SORTED;
+    return COLOR_UNSORTED
+}
+
+
+const SelectionSort = ({ initValues, isAsc, }: { initValues: number[], isAsc: boolean, }) => {
+    const { setSortCompleted, audioClickRef, audioTransRef, audioMarkRef } = useContext(MainContext)
+    const { isCompleted, nextStep, values, makeSwap } = useSelectionSort(initValues, isAsc)
+
+    const arrowRef = useRef<{ p1: HTMLDivElement | null, p2: HTMLDivElement | null }>({ p1: null, p2: null })
+
+    useEffect(() => {
+        setSortCompleted(isCompleted)
+    }, [isCompleted])
+
+    useEffect(() => {
+        if (values.findIndex(v => v.isMarkedForSwap) > -1) {
+            if (makeSwap)
+                audioTransRef.current.play()
+            else
+                audioMarkRef.current.play()
+        }
+        else {
+            audioClickRef.current.play().catch(res => {
+                // console.log(res)
+            })
+
+        }
+    }, [values])
+
+    useEffect(() => {
+        setSortCompleted(isCompleted)
+    }, [isCompleted])
+
+    const p1Index = values.findIndex(v => v.isMarkedForSwap)//p1 index
+    const sondSlice = values.slice(p1Index + 1)
+    const p2Index = sondSlice.findIndex(v => v.isMarkedForSwap) + p1Index + 1//p2 index
+    let totalEleHeight = 0
+    if (arrowRef.current.p1) {
+        const eleStyle = window.getComputedStyle(arrowRef.current.p1)
+        totalEleHeight += arrowRef.current.p1.offsetHeight
+        totalEleHeight += parseFloat(eleStyle.marginTop)
+    }
+
+    return (
+        <div className="pageRender  h-full p-2 px-10 text-white">
+            <div className="content h-[75vh] overflow-auto relative ">{values.map((element, index) => {
+                let props = { transform: "", };
+                const { p1, p2 } = arrowRef.current;
+                // let p2Index = -1;
+                if (element.isMarkedForSwap && p1 && p2) {
+                    let pv = p2.offsetTop - p1.offsetTop
+                    console.log("p:", p1, p1.innerText, pv, "p2:", p2, p2.innerText)
+                    // p2Index = index;
+                    if (makeSwap) {
+                        if (p1Index !== index) {//p2
+                            // .getBoundingClientRect().y
+                            props.transform = `translateY(${-pv}px)`
+                            // else //p1
+                        }
+                    }
+                }
+                if (makeSwap && index >= p1Index && index < p2Index) {
+                    props.transform = `translateY(${totalEleHeight}px)`
+                }
+                return <div key={index} style={{
+                    width: `${element.value}%`, backgroundColor: `${getColor(element)}`,
+                    ...props
+                }}
+                    ref={r => {
+                        if (element.isMarkedForSwap)
+                            if (values.findIndex(v => v.isMarkedForSwap) === index)
+                                arrowRef.current.p1 = r
+                            else
+                                arrowRef.current.p2 = r
+                    }}
+                    className={"value px-2 py-1 select-none bg-[#E03B8B] rounded-r text-[1.5rem]  my-3 " + (element.isMarkedForSwap ? "transition-transform duration-300" : "")}>{element.value}</div>
+            })}
+            </div>
+            <Controls nextStep={nextStep} />
+
+        </div>
+    );
+}
+
+export default SelectionSort;
---
+++ app/hooks/useSelectionSort.ts
@@ -0,0 +1,105 @@
+import { useEffect, useState } from "react"
+import { convertToElementType } from "~/helpers";
+import type { ElementType } from "~/types";
+
+let FirstRender = true;
+
+const useSelectionSort = (arr: number[], isAsc: boolean) => {
+    const [flagI, setFlagI] = useState(0)
+    const [flagJ, setFlagJ] = useState(0);
+    const [smallIdx, setSmallIdx] = useState(0);
+    const [isCompleted, setIsCompleted] = useState(false);
+    const [makeSwap, setMakeSwap] = useState(false);
+    const [values, setValues] = useState<ElementType[]>(() => convertToElementType(arr))
+
+    useEffect(() => {
+        // reset State
+        setValues(convertToElementType(arr))
+        setFlagI(0)
+        setFlagJ(0)
+        setSmallIdx(0)
+        // flagJ will be reset by useEffect
+        setIsCompleted(false)
+    }, [isAsc, arr])
+
+    useEffect(() => {
+        setValues(preValues => preValues.map((val, i) => ({ ...val, IsSorted: flagI > i })))
+    }, [flagI])
+
+    useEffect(() => {
+        setValues(preValues => preValues.map((val, i) => ({ ...val, isMarkedForCompare: flagJ === i || smallIdx === i })))
+    }, [flagJ, smallIdx])
+
+    useEffect(() => {
+        if (FirstRender)
+            FirstRender = false
+        else {
+            setSmallIdx(flagI)
+            setFlagJ(flagI + 1)
+        }
+    }, [flagI])
+
+
+    const nextFunction = (values: ElementType[]) => {
+        if (flagI >= values.length - 1) {
+            setFlagI(pre => pre + 1)
+            setIsCompleted(true)
+            return [...values]
+        }
+
+        // Step 1: Compare and update smallIdx
+        if (flagJ < values.length) {
+            if (isAsc ? values[flagJ].value < values[smallIdx].value : values[flagJ].value > values[smallIdx].value) {
+                setSmallIdx(flagJ)
+            }
+            setFlagJ(j => j + 1)
+            return [...values]
+        }
+
+        // Step 2: End of inner loop, check if swap needed
+        if (flagJ === values.length) {
+            if (smallIdx !== flagI) {
+                // Mark for swap
+                values[flagI].isMarkedForSwap = true
+                values[smallIdx].isMarkedForSwap = true
+                if (makeSwap) {
+                    // Do swap
+                    values[flagI].isMarkedForSwap = false
+                    values[smallIdx].isMarkedForSwap = false
+
+                    const [removed] = values.splice(smallIdx, 1)
+                    values.splice(flagI, 0, removed)
+                    // Reset flags
+                    setMakeSwap(false)
+                    setFlagI(i => i + 1)
+                    setFlagJ(flagI + 1)
+                    setSmallIdx(flagI)
+                } else {
+                    setMakeSwap(true)
+                }
+            } else {
+                // No swap needed, move to next
+                setFlagI(i => i + 1)
+                setFlagJ(flagI + 1)
+                setSmallIdx(flagI)
+            }
+        }
+
+        return [...values]
+    }
+
+    return {
+        isCompleted,
+        values,
+        flagI,
+        flagJ,
+        smallIdx,
+        makeSwap,
+        nextStep() {
+            const updatedValues = nextFunction(values)
+            setValues(updatedValues)
+        }
+    };
+}
+
+export default useSelectionSort;
